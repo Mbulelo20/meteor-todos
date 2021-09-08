b35e4b7b2f6071fff0040c2e983c26a465d0b458
Mbulelo Pani <mbulelopani09@gmail.com>
2021-09-08 12:02:36 +0200

    Form component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Task } from './Task.jsx';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { Task } from './Task';
+import { TaskForm } from './TaskForm';
 
 const tasks = [
   {_id: 1, text: 'First Task'},
   {_id: 2, text: 'Second Task'},
   {_id: 3, text: 'Third Task'},
 ];
-export const App = () => (
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+
+  return (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <TaskForm />
     <ul>
       { tasks.map(task => <Task key={ task._id } task={ task }/>) }
     </ul>
     
-  </div>
-);
+  </div>)
+}
